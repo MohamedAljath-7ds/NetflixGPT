3674dec325ec9a4681c311141ae235c068b82f8a
Aljath <mohamedaljathn@gmail.com>
2024-03-29 15:05:39 +0530

    Actually implement useNavigate hooke to navigate to the Browse page if the user is authenticated

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -2,8 +2,16 @@ import Login from "./Login"
 import Browse from "./Browse"
 import {createBrowserRouter} from "react-router-dom"
 import { RouterProvider } from "react-router-dom"
+import { useEffect } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "../utils/firebase"
+import { Provider, useDispatch } from "react-redux"
+import { RemoveUser, addUser } from "../utils/userSlice"
+import appStore from "../utils/appstore"
+
 
 const Body = () => {
+ const dispatch = useDispatch()
  const appRouter = createBrowserRouter([
   {
     path:"/",
@@ -14,9 +22,27 @@ const Body = () => {
     element:<Browse/>
   }
  ])
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid, email, displayName} = user;
+        dispatch(addUser({uid:uid, email:email, displayName:displayName}));
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(RemoveUser());
+      }
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
   return (
     <>
         <RouterProvider router={appRouter}/>
+        
     </>
   )
 }
