0dec0a3ef8661ca7eddb2d0d24a7b69357479400
Aljath <mohamedaljathn@gmail.com>
2024-03-29 15:04:02 +0530

    provide redux store for managing data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux"
 import Body from "./component/Body"
+import appStore from "./utils/appstore"
 
 function App() {
 
 
   return (
     <>
+    <Provider store={appStore}>
      <Body/>
+     </Provider>
     </>
   )
 }
